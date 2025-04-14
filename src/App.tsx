864d15b3b75df6a83f8af38f1ada71d397233cd7
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2025-04-14 11:03:00 +0200

    feat: initialize project with Vite and React setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Input } from '@mui/joy';
+
+function App() {
+	return (
+		<>
+			<h1>Bouvet Island Weather Service</h1>
+			<Input></Input>
+		</>
+	);
+}
+
+export default App;
