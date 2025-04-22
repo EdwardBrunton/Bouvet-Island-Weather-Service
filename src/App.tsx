2249bb7543bd13cd4798b2908963398621be6b7a
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2025-04-22 18:38:12 +0200

    fix: update coordinates to Bouvet Island for accurate weather data

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ const StyledContainer = styled.div`
 `;
 
 function App() {
-	// coordinates for stavanger
-	const latitude = 58.97;
-	const longitude = 5.73;
+	// coordinates for Bouvet Island
+	const latitude = -54.392;
+	const longitude = 3.355;
 
 	const forecast = useForecastQuery(latitude, longitude);
 
